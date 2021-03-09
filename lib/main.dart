6b1049f630ade251bf195075be71ca404d1dda94
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2021-03-10 01:53:39 +0800

    Done project

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,36 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/karl.jpg'),
+              ),
+              Text(
+                'Karl Marx Manzano',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 28.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FULL STACK DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 16.0,
+                  color: Colors.white70,
+                  letterSpacing: 1.10,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
